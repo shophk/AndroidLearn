54887d56dc496e25e37e22bd24d406b4c9d5cb74
Mike <mike@>
2019-06-20 21:11:58 +0800

    initial commit.

---
+++ training/kotlinAndroid/_1DiceRoller/app/src/main/java/com/example/scrolltextview/diceroller/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.scrolltextview.diceroller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton : Button = findViewById(R.id.roll_button)
+        rollButton.text = "Let's roll"
+    }
+}
