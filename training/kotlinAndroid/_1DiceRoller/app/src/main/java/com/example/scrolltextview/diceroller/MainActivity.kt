2ab4148f30e31dc0f23eb8ea0153a9977e63dc15
Mike <mike@>
2019-06-24 23:09:31 +0800

    Added kotlin for android testing onclicklistener.

--- training/kotlinAndroid/_1DiceRoller/app/src/main/java/com/example/scrolltextview/diceroller/MainActivity.kt
+++ training/kotlinAndroid/_1DiceRoller/app/src/main/java/com/example/scrolltextview/diceroller/MainActivity.kt
@@ -2,15 +2,23 @@ package com.example.scrolltextview.diceroller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_constraint)
 
         val rollButton : Button = findViewById(R.id.roll_button)
         rollButton.text = "Let's roll"
+
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "ROLLING IT", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 }
